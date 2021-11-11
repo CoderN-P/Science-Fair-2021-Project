6a1800604d18cad75b5b2888d471f09b74839baf
CoolCoderN <76001641+CoolCoderN@users.noreply.github.com>
2021-11-10 22:00:43 -0800

    added json for collecting soil moisture data

--- soil_moisture.py
+++ soil_moisture.py
@@ -5,6 +5,7 @@ from mongo_methods import update_readings
 from adafruit_seesaw.seesaw import Seesaw
 from plant_watering import water_plant
 from mongo_methods import get_device
+import json, datetime
 
 i2c_bus = busio.I2C(SCL, SDA)
 
@@ -15,6 +16,7 @@ class SoilMoisture:
     def __init__(self):
         self.soil_moisture = None
         self.temperature = None
+        self.data_num = 0
 
     def background_reading(self):
         while True:
@@ -22,8 +24,18 @@ class SoilMoisture:
             print(self.soil_moisture)
             update_readings(self.soil_moisture, self.temperature)
             self.temperature = ss.get_temp()
+            if self.data_num % 180 == 0:
+                file = open("data.json", "w")
+                data = json.load(file)
+                data["soil_moisture_per_30_interval"].append(self.soil_moisture)
+                json.dump(data, file)
+            self.data_num += 1
             if self.soil_moisture < 800:
                 seconds = get_device()["seconds_to_water"]
                 water_plant(seconds)
+                file = open("data.json", "w")
+                data = json.load(file)
+                data["times_watered"].append(datetime.datetime.now().isoformat())
+                json.dump(data, file)
 
             time.sleep(10)
