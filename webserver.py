76355599e3f9bbab1fb012134444551f492cd79d
CoderN-P <neel.parpia@gmail.com>
2022-03-14 19:46:43 -0700

    (Actually) Added CORS to the webserver

--- webserver.py
+++ webserver.py
@@ -1,11 +1,13 @@
 import flask
 from plant_watering import water_plant
 from mongo_methods import get_device
+from flask_cors import cross_origin
 
 app = flask.Flask(__name__)
 
 
 @app.route("/webhook", methods="GET")
+@cross_origin()
 def webhook():
     seconds = get_device()["seconds_to_water"]
     water_plant(seconds)
